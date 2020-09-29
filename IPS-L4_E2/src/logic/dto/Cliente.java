bf0bffa89071eec5bc1ca2e91304a078e17e8cf4
aleja <aleja@DESKTOP-QK5H09T>
2020-09-29 16:57:15 +0200

    Objeto cliente

---
+++ IPS-L4_E2/src/logic/dto/Cliente.java
@@ -0,0 +1,39 @@
+package logic.dto;
+
+public class Cliente {
+	private long ID;
+	private String Nombre;
+	private String Apellidos;
+	private double Telefono;
+	public Cliente(long iD, String nombre, String apellidos, double telefono) {
+		super();
+		ID = iD;
+		Nombre = nombre;
+		Apellidos = apellidos;
+		Telefono = telefono;
+	}
+	public long getID() {
+		return ID;
+	}
+	public void setID(long iD) {
+		ID = iD;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getApellidos() {
+		return Apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		Apellidos = apellidos;
+	}
+	public double getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(double telefono) {
+		Telefono = telefono;
+	}
+}
