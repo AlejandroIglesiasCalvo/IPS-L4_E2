c6504bd0870fcbcbaedb7d5112074b88d5f16477
aleja <aleja@DESKTOP-QK5H09T>
2020-09-29 16:54:14 +0200

    Creo el objeto producto

---
+++ IPS-L4_E2/src/logic/dto/Producto.java
@@ -0,0 +1,49 @@
+package logic.dto;
+
+public class Producto {
+	private long ID;
+	private String Nombre;
+	private String Tipo;
+	private double Precio;
+
+	public Producto(long iD, String nombre, String tipo, double precio) {
+		super();
+		ID = iD;
+		Nombre = nombre;
+		Tipo = tipo;
+		Precio = precio;
+	}
+
+	public long getID() {
+		return ID;
+	}
+
+	public void setID(long iD) {
+		ID = iD;
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public String getTipo() {
+		return Tipo;
+	}
+
+	public void setTipo(String tipo) {
+		Tipo = tipo;
+	}
+
+	public double getPrecio() {
+		return Precio;
+	}
+
+	public void setPrecio(double precio) {
+		Precio = precio;
+	}
+
+}
