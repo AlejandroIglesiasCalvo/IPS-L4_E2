d28cd5f95e07d461f20f0cd1798ce219294dab20
aleja <aleja@DESKTOP-QK5H09T>
2020-09-29 16:56:10 +0200

    Onjeto repartidor

---
+++ IPS-L4_E2/src/logic/dto/Repartidor.java
@@ -0,0 +1,32 @@
+package logic.dto;
+
+public class Repartidor {
+private long ID;
+private String Nombre;
+private double Telefono;
+public Repartidor(long iD, String nombre, double telefono) {
+	super();
+	ID = iD;
+	Nombre = nombre;
+	Telefono = telefono;
+}
+public long getID() {
+	return ID;
+}
+public void setID(long iD) {
+	ID = iD;
+}
+public String getNombre() {
+	return Nombre;
+}
+public void setNombre(String nombre) {
+	Nombre = nombre;
+}
+public double getTelefono() {
+	return Telefono;
+}
+public void setTelefono(double telefono) {
+	Telefono = telefono;
+}
+
+}
