6a3987440396e14f01de9bf994f2b708f04941c7
ALEJANDRO IGLESIAS CALVO <UO247346@uniovi.es>
2020-10-06 18:51:04 +0200

    Clase de gestion de fechas

--- IPS-L4_E2/src/logic/EntregaController.java
+++ IPS-L4_E2/src/logic/EntregaController.java
@@ -1,5 +1,26 @@
 package logic;
 
+import java.time.LocalDateTime;
+
+import logic.dto.Presupuesto;
+import logic.dto.Transporte;
+
 public class EntregaController {
+	private Presupuesto presupuesto;
+	private Transporte transporte;
+	private gestionFechas fecha;
+
+	public EntregaController(Presupuesto presupuesto) {
+		super();
+		this.presupuesto = presupuesto;
+	}
+	//Mira si la fecha no es domingo y esta en horario de 8 a 22
+	public boolean es_fecha_valida(LocalDateTime fecha_propuesta) {
+		fecha = new gestionFechas(fecha_propuesta);
+		if (fecha.no_es_domingo() && fecha.esta_en_horario()) {
+			return true;
+		}
+		return false;
+	}
 
 }
---
+++ IPS-L4_E2/src/logic/gestionFechas.java
@@ -0,0 +1,73 @@
+package logic;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class gestionFechas {
+	private LocalDateTime fecha;
+	private LocalTime hora;
+	private LocalTime entrada = LocalTime.of(8, 00);
+	private LocalTime salida = LocalTime.of(20, 00);
+
+	/**
+	 * Se crea asi: LocalDateTime localDateTimeOf = LocalDateTime.of(2017,
+	 * Month.AUGUST, 20, 8, 30);
+	 * 
+	 */
+	public gestionFechas(LocalDateTime fecha) {
+		super();
+		this.fecha = fecha;
+		this.hora = LocalTime.of(fecha.getHour(), 00);
+	}
+
+	/** Devuelve la fecha en formato entero */
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	/** Cambia la fecha en formato entero */
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	/** Devuelve la hora de inicio de los repartos */
+	public LocalTime getEntrada() {
+		return entrada;
+	}
+
+	/** Devuelve la hora de finalizacion de los repartos */
+	public LocalTime getSalida() {
+		return salida;
+	}
+
+	/** Devuelve la hora de la fecha dada, en formato LocalTime (horas, minutos) */
+	public LocalTime getHoraDeFecha() {
+		LocalTime hora = LocalTime.of(fecha.getHour(), fecha.getMinute());
+		return hora;
+	}
+
+	/** Devuelve el dia de la semana de la fecha */
+	public DayOfWeek getDiaSemana() {
+		DayOfWeek dia = fecha.getDayOfWeek();
+		return dia;
+	}
+
+	/** Comprueba que no es domingo */
+	public boolean no_es_domingo() {
+
+		if (fecha.getDayOfWeek() != DayOfWeek.WEDNESDAY) {
+			return true;
+		}
+		return false;
+	}
+
+	/** Esta en horario de reparto */
+	public boolean esta_en_horario() {
+
+		if (hora.isAfter(entrada) && hora.isBefore(salida)) {
+			return true;
+		}
+		return false;
+	}
+}
