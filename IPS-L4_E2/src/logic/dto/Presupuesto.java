6b3fccf89338adb90110d3426a513eef0b8b26b7
ALEJANDRO IGLESIAS CALVO <UO247346@uniovi.es>
2020-10-06 17:37:41 +0200

    Agregado el presupuesto DTO y empiezo con el controller de la entrega

---
+++ IPS-L4_E2/src/logic/dto/Presupuesto.java
@@ -0,0 +1,52 @@
+package logic.dto;
+
+import java.sql.Date;
+
+public class Presupuesto {
+
+	private long ID_Presupuesto;
+	private int DNI_Cliente;
+	private Date fecha;
+	private double precio;
+
+	public Presupuesto(long iD_Presupuesto, int dNI_Cliente, Date fecha, double precio) {
+		super();
+		ID_Presupuesto = iD_Presupuesto;
+		DNI_Cliente = dNI_Cliente;
+		this.fecha = fecha;
+		this.precio = precio;
+	}
+
+	public long getID_Presupuesto() {
+		return ID_Presupuesto;
+	}
+
+	public void setID_Presupuesto(long iD_Presupuesto) {
+		ID_Presupuesto = iD_Presupuesto;
+	}
+
+	public int getDNI_Cliente() {
+		return DNI_Cliente;
+	}
+
+	public void setDNI_Cliente(int dNI_Cliente) {
+		DNI_Cliente = dNI_Cliente;
+	}
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+}
