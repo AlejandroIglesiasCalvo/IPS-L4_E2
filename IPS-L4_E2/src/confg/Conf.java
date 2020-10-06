f8119405df2f9d5389e4dac99a280d47c4e73d86
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2020-10-06 11:49:30 +0200

    Arreglada la conexiona la bbdd

--- IPS-L4_E2/src/confg/Conf.java
+++ IPS-L4_E2/src/confg/Conf.java
@@ -5,37 +5,36 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Conf {
-	
+
 	private static Conf instance;
 	private Properties properties;
-	private final static String CONF_FILE ="resources/configuration.properties";
-	
-			private Conf(){
+	private final static String CONF_FILE = "resources/configuration.properties";
+
+	private Conf() {
 		properties = new Properties();
-		try{
+		try {
 			final ClassLoader loader = getClass().getClassLoader();
 			InputStream config = loader.getResourceAsStream(CONF_FILE);
 			properties.load(config);
-		}
-		catch(IOException e){
+		} catch (IOException e) {
 			throw new RuntimeException("Porperties file can not loader", e);
 		}
 	}
-	
-	public static String get(String key){
+
+	public static String get(String key) {
 		return getInstance().getProperty(key);
 	}
-	
-	private String getProperty(String key){
+
+	private String getProperty(String key) {
 		String value = properties.getProperty(key);
-		if(value==null){
+		if (value == null) {
 			throw new RuntimeException("Porperties not found in config file");
 		}
 		return value;
 	}
 
 	private static Conf getInstance() {
-		if(instance==null){
+		if (instance == null) {
 			instance = new Conf();
 		}
 		return instance;
