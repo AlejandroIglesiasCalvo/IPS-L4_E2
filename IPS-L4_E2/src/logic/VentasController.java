b8716e3b098d9550e82d3e6c8f60be1e79ceeabe
Alejandro Iglesias Calvo <47303435+UO247346@users.noreply.github.com>
2020-11-02 20:06:44 +0100

    Mañana mas

---
+++ IPS-L4_E2/src/logic/VentasController.java
@@ -0,0 +1,14 @@
+package logic;
+
+import java.util.List;
+
+import logic.dto.Venta;
+
+public class VentasController {
+
+	public List<Venta> getTodasLasVentas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- IPS-L4_E2/src/ui/Ventas/ListarVentas.java
+++ IPS-L4_E2/src/ui/Ventas/ListarVentas.java
@@ -3,13 +3,37 @@ package ui.Ventas;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import logic.VentasController;
+import logic.dto.ProductoCarrito;
+import logic.dto.Venta;
+
+import java.awt.GridLayout;
+import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JScrollPane;
+
 public class ListarVentas extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JLabel lblTitulo;
+	private JScrollPane scrollVentas;
+	private JList<String> listVentas;
+	private List<Venta> Ventas;
+	private VentasController vc;
 
 	/**
 	 * Launch the application.
@@ -31,12 +55,55 @@ public class ListarVentas extends JFrame {
 	 * Create the frame.
 	 */
 	public ListarVentas() {
+		vc = new VentasController();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 671, 492);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.add(getLblTitulo(), BorderLayout.NORTH);
+		contentPane.add(getScrollVentas(), BorderLayout.CENTER);
+
+	}
+
+	private JLabel getLblTitulo() {
+		if (lblTitulo == null) {
+			lblTitulo = new JLabel("Hisotrial de ventas");
+			lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+			lblTitulo.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		}
+		return lblTitulo;
+	}
+
+	private JScrollPane getScrollVentas() {
+		if (scrollVentas == null) {
+			scrollVentas = new JScrollPane();
+			scrollVentas.setViewportView(getListVentas());
+		}
+		return scrollVentas;
+	}
+
+	private DefaultListModel<String> modelListVentas() {
+		DefaultListModel<String> model = new DefaultListModel<String>();
+		for (Venta or : getListaProductoCarritosTienda())
+			model.addElement(or.getID() + "  " + or.getFecha() + "  " + or.getTotal());
+		return model;
+	}
+
+	private List<Venta> getListaProductoCarritosTienda() {
+		Ventas = vc.getTodasLasVentas();
+		return Ventas;
+
+	}
+
+	private JList<String> getListVentas() {
+		if (listVentas == null) {
+			listVentas = new JList<String>();
+			listVentas.setBounds(10, 63, 644, 261);
+			listVentas.setModel(modelListVentas());
+		}
+		return listVentas;
 	}
 
 }
