d2ca15657fde77bf70153d3c90847299588bd036
uo269570 <uo269570@uniovi.es>
2020-11-23 18:07:51 +0100

    hacer interfaz grafica

--- IPS-L4_E2/src/ui/grafico/VisualizarGrafico.java
+++ IPS-L4_E2/src/ui/grafico/VisualizarGrafico.java
@@ -6,10 +6,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.GridLayout;
+import javax.swing.JComboBox;
+import java.awt.Font;
 
 public class VisualizarGrafico extends JFrame {
 
 	private JPanel contentPane;
+	private JPanel pnSeleccionGrafico;
+	private JPanel pnGrafico;
+	private JLabel lblMes;
+	private JComboBox comboBox;
 
 	/**
 	 * Launch the application.
@@ -32,11 +41,46 @@ public class VisualizarGrafico extends JFrame {
 	 */
 	public VisualizarGrafico() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 531, 377);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.add(getPnSeleccionGrafico(), BorderLayout.NORTH);
+		contentPane.add(getPnGrafico(), BorderLayout.CENTER);
 	}
 
+	private JPanel getPnSeleccionGrafico() {
+		if (pnSeleccionGrafico == null) {
+			pnSeleccionGrafico = new JPanel();
+			pnSeleccionGrafico.setBackground(Color.WHITE);
+			pnSeleccionGrafico.setLayout(new GridLayout(1, 0, 3, 3));
+			pnSeleccionGrafico.add(getLblMes());
+			pnSeleccionGrafico.add(getComboBox());
+		}
+		return pnSeleccionGrafico;
+	}
+	private JPanel getPnGrafico() {
+		if (pnGrafico == null) {
+			pnGrafico = new JPanel();
+			pnGrafico.setBackground(Color.WHITE);
+		}
+		return pnGrafico;
+	}
+	private JLabel getLblMes() {
+		if (lblMes == null) {
+			lblMes = new JLabel("Seleccionar mes :");
+			lblMes.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		}
+		return lblMes;
+	}
+	private JComboBox getComboBox() {
+		if (comboBox == null) {
+			comboBox = new JComboBox();
+			comboBox.setFont(new Font("Tahoma", Font.PLAIN, 16));
+			comboBox.setBackground(Color.WHITE);
+		}
+		return comboBox;
+	}
 }
